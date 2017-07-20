3bab4b6b47083e03b328c0530854bb393110f94c
Yuriy Baev <yuriy.baev@nixsolutions.com>
2017-07-20 11:34:39 +0300

    Added random bg to project

---
+++ src/assets/images/icons/index.js
@@ -0,0 +1,36 @@
+import angular from './angular.svg';
+import CSS from './css.svg';
+import go from './go.svg';
+import cPlusPlus from './c_plus_plus.svg';
+import gulp from './gulp.svg';
+import java from './java.svg';
+import jQuery from './jQuery.svg';
+import js from './js.svg';
+import node from './node.svg';
+import react from './react.svg';
+import ruby from './ruby.svg';
+import swift from './swift.svg';
+import WordPress from './WordPress.svg';
+import sass from './sass.svg';
+import html from './html.svg';
+import TypeScript from './typescript.svg';
+
+const images = {
+  angular,
+  CSS,
+  cPlusPlus,
+  go,
+  gulp,
+  java,
+  jQuery,
+  js,
+  node,
+  react,
+  ruby,
+  swift,
+  WordPress,
+  sass,
+  html,
+  TypeScript,
+};
+export default images;
